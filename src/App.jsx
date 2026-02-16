f599c81177154016b73019f648ccf7050b5cb771
Thierru <thierry.p057@gmail.com>
2026-02-16 17:19:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import { supabase } from "./lib/supabaseClient";
+
 export default function App() {
+
+  useEffect(() => {
+    const testConnection = async () => {
+      const { data, error } = await supabase.from("produits").select("*");
+      console.log("DATA:", data);
+      console.log("ERROR:", error);
+    };
+
+    testConnection();
+  }, []);
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">EMASI STOCK</h1>
-      <p>Application prête à connecter à Supabase</p>
+      <p>Connexion Supabase en cours...</p>
     </div>
-  )
+  );
 }
